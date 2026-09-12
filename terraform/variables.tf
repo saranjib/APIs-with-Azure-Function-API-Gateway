a5f50b0089f155840c9a09eada15a27a9e3fce6e
Saranjib Kuanar <102132535+saranjib@users.noreply.github.com>
2026-09-12 19:36:45 +0530

    Update default resource group name in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,6 +7,6 @@ variable "location" {
 variable "resource_group_name" {
   description = "Resource group name"
   type        = string
-  default     = "rg-functions-apim"
+  default     = "rg-function-api-project"
 }
 
