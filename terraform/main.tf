c9dd050936cf0a62bd4a30844ccdcc8836b88a29
Saranjib Kuanar <102132535+saranjib@users.noreply.github.com>
2026-09-13 21:07:47 +0530

    Add Azure Storage Account resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,3 +4,15 @@ resource "azurerm_resource_group" "rg-function-api-project" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "azurerm_storage_account" "function_storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg-function-api-project.name
+  location                 = azurerm_resource_group.rg-function-api-project.location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+
+  min_tls_version = "TLS1_2"
+}
+
+
